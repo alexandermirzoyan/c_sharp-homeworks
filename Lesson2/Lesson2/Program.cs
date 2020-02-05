c0e391e84c6e7d3ab50c4059c1e1ffb909cd703a
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2020-02-05 21:43:20 +0400

    Added Lesson2 homework

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program {
+        static void Main(string[] args) {
+            /*
+            ====Test code====
+            Console.Write("Input string:::");
+            byte a = Convert.ToByte(Console.ReadLine());
+            byte b = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine(a + b);
+            Console.WriteLine("Hello World!");
+            */
+
+            // Rectangle
+            Console.WriteLine("******");
+            Console.WriteLine("*    *");
+            Console.WriteLine("*    *");
+            Console.WriteLine("*    *");
+            Console.WriteLine("*    *");
+            Console.WriteLine("******");
+            // ======
+            Console.WriteLine();
+            // Triangle
+            Console.WriteLine("    *    ");
+            Console.WriteLine("   ***   ");
+            Console.WriteLine("  *****  ");
+            Console.WriteLine(" ******* ");
+            Console.WriteLine("*********");
+            // ======
+            Console.WriteLine();
+            // Circle
+            Console.WriteLine("      **********       ");
+            Console.WriteLine("   *****      *****    ");
+            Console.WriteLine(" ****            ****  ");
+            Console.WriteLine("***                *** ");
+            Console.WriteLine("***                *** ");
+            Console.WriteLine("***                *** ");
+            Console.WriteLine(" ****            ****  ");
+            Console.WriteLine("   *****      *****    ");
+            Console.WriteLine("      **********       ");
+            // ======
+            Console.WriteLine();
+            // Square
+            Console.WriteLine("* * * * *");
+            Console.WriteLine("* * * * *");
+            Console.WriteLine("* * * * *");
+            Console.WriteLine("* * * * *");
+            Console.WriteLine("* * * * *");
+            // ======
+            Console.WriteLine();
+            // Shexankyun
+            Console.WriteLine("**********");
+            Console.WriteLine(" **********");
+            Console.WriteLine("  **********");
+            Console.WriteLine("   **********");
+            Console.WriteLine("    **********");
+            // ======
+        }
+    }
+}
