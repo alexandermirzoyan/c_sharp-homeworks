cddd64c08cd559766fc9690bfd8f233e17e6591b
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2020-02-10 21:40:43 +0400

    Lesson3 Homework

---
+++ Lesson3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 4 digit number");
+            Console.Write("Number = ");
+            int inputedNumber = Convert.ToInt32(Console.ReadLine());
+            int haz = inputedNumber / 1000;
+            int har = inputedNumber / 100 % 10;
+            int tas = inputedNumber / 10 % 10;
+            int miav = inputedNumber % 10;
+
+            int min = miav;
+
+            Console.WriteLine($"Hazaravor = {haz}");
+            Console.WriteLine($"Haryuravor = {har}");
+            Console.WriteLine($"Tasnavor = {tas}");
+            Console.WriteLine($"Miavor = {miav}");
+
+            if (min > tas) {
+                min = tas;
+            }
+            if (min > har) {
+                min = har;
+            }
+            if (min > haz) {
+                min = haz;
+            }
+
+            Console.WriteLine($"Min value = {min}");
+        }
+    }
+}
