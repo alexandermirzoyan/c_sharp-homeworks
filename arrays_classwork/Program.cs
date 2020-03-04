f53874846d7ca7c5bedadcacb23c85ec12cc351e
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2020-03-04 23:23:48 +0400

    array homework done

---
+++ arrays_classwork/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace arrays_classwork {
+    class Program {
+        static void Main(string[] args) {
+            /*
+            Console.Write("Input length of array = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++) {
+                arr[i] = random.Next(-100,100);
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+            int max = arr[0], min = arr[0];
+            int maxIndex = 0, minIndex = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                if (max < arr[i]) {
+                max = arr[i];
+                maxIndex = i;
+                }
+                if (min > arr[i]) {
+                min = arr[i];
+                minIndex = i;
+                }
+            }
+        ​
+            Console.WriteLine();
+            Console.WriteLine("Before swaping");
+            Console.WriteLine($"Max = {max}, Min = {min}");
+        ​
+            Console.WriteLine();
+            Console.WriteLine("After swaping");
+            int tmp = max;
+            max = min;
+            min = tmp;
+            Console.WriteLine($"Max = {max}, Min = {min}");
+        ​
+            int tmp2 = arr[maxIndex];
+            arr[maxIndex] = arr[minIndex];
+            arr[minIndex] = tmp2;
+        ​
+            for (int i = 0; i < arr.Length; i++) {
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+        ​
+            Console.Write("Input length of array = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random random = new Random();
+        ​
+            for (int i = 0; i < arr.Length; i++) {
+                int randomNumber = random.Next(1,20);
+                for (int j = 2; j < randomNumber/2; j++) {
+                if (randomNumber % 2 != 0) {
+                    randomNumber = 0;
+                }
+                }
+                arr[i] = randomNumber;
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+            */
+​
+            Console.Write("Please, input length of array = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random random = new Random();
+            int t;
+            Console.WriteLine("Original array :");
+            for (int i = 0; i < arr.Length; i++) {
+                arr[i] = random.Next(1, 10);
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+            for (int i = 0; i <= arr.Length; i++) {
+                for (int j = 0; j <= arr.Length - 2; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        t = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = t;
+                    }
+                }
+            }
+            Console.WriteLine("\n" + "Sorted array :");
+            for (int i = 0; i < arr.Length; i++) {
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+
+        }
+    }
+}
---
+++ arrays_homework/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace arrays_homework {
+    class Program {
+
+        static void xndir1() {
+            Console.Write("Please, input length of array = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random random = new Random();
+            int negativeNumberCount = 0;
+
+            // ete 0 tanq, kara negativ tiv chlini u asi vor negativi index@ = 0. Dra hamar talis enq null
+            Nullable<int> negativeElementIndex = null;
+
+            int randomNumberMin = -100;
+            int randomNumberMax = 100;
+
+            for (int i = 0; i < arr.Length; i++) {
+                int randomNumber = random.Next(randomNumberMin, randomNumberMax);
+                arr[i] = randomNumber;
+                if (arr[i] < 0) {
+                    randomNumberMin = 0;
+                    negativeElementIndex = i;
+                    negativeNumberCount++;
+                }
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+            Console.WriteLine($"Bacasakan tveri qanak = {negativeNumberCount}");
+            if (negativeElementIndex != null) {
+                Console.WriteLine($"Bacasakan arjeqi index@ = {negativeElementIndex}");
+            }
+        }
+
+        static void xndir2() {
+            Console.Write("Please, input length of array = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random random = new Random();
+            int sumOfEvenIndexes = 0;
+            int sumOfEvenIndexesElements = 0;
+
+            for (int i = 0; i < arr.Length; i++) {
+                arr[i] = random.Next(-100, 100);
+                if (i % 2 == 0) {
+                    sumOfEvenIndexes += i;
+                    sumOfEvenIndexesElements += arr[i];
+                }
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+
+            Console.WriteLine($"Zuyg index unecoxneri gumar@ = {sumOfEvenIndexes}");
+            Console.WriteLine($"Zuyg index unecoxneri elementneri gumar@ = {sumOfEvenIndexesElements}");
+        }
+
+        static void xndir3() {
+            Console.Write("Please, input length of array = ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> arr = new List<int>();
+            List<int> chkrknvoxArr = new List<int>();
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++) {
+                arr.Add(random.Next(10, 20));
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+
+            for (int i = 0; i < n; i++) {
+                int krknvox = 0;
+                for (int j = 0; j < i - 1; j++) {
+                    if (arr[i] == arr[j]) {
+                        krknvox++;
+                    }
+                }
+                for (int k = i + 1; k < n; k++) {
+                    if (arr[i] == arr[k]) {
+                        krknvox++;
+                    }
+                    if (arr[i] == arr[i+1]) {
+                        i++;
+                    }
+                }
+                if(krknvox == 0) {
+                    chkrknvoxArr.Add(arr[i]);
+                }
+            }
+
+            for (int i = 0; i < chkrknvoxArr.Count; i++) {
+                Console.WriteLine($"chkrknvoxArr[{i}] = {chkrknvoxArr[i]}");
+            }
+        }
+
+        static void Main(string[] args) {
+            xndir1();
+            //xndir2();
+            //xndir3();
+        }
+    }
+}
